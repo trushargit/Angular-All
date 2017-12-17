eb9e6cfb2e7e53d8752700e59c3ad7e7b6516764
trushargit <trushargit@gmail.com>
2017-12-17 16:26:13 -0600

    first
    commit this

---
+++ angular-2-reactive-forms/03-building-a-reactive-form/Demo/app/customers/customer.component.js
@@ -0,0 +1,48 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require("@angular/core");
+var forms_1 = require("@angular/forms");
+var customer_1 = require("./customer");
+var CustomerComponent = (function () {
+    function CustomerComponent(fb) {
+        this.fb = fb;
+        this.customer = new customer_1.Customer();
+    }
+    CustomerComponent.prototype.ngOnInit = function () {
+        this.customerForm = this.fb.group({
+            firstName: '',
+            lastName: '',
+            email: '',
+            sendCatalog: true
+        });
+    };
+    CustomerComponent.prototype.populateTestData = function () {
+        this.customerForm.patchValue({
+            firstName: 'Jack',
+            lastName: 'Harkness',
+            sendCatalog: false
+        });
+    };
+    CustomerComponent.prototype.save = function () {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    };
+    return CustomerComponent;
+}());
+CustomerComponent = __decorate([
+    core_1.Component({
+        selector: 'my-signup',
+        templateUrl: './app/customers/customer.component.html'
+    }),
+    __metadata("design:paramtypes", [forms_1.FormBuilder])
+], CustomerComponent);
+exports.CustomerComponent = CustomerComponent;
+//# sourceMappingURL=customer.component.js.map
